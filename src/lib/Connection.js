2acf70eee73d28de5ccb5988c7a979f30464c27f
Andrew Wong <andrew.j.wong@outlook.com>
2019-06-25 21:49:00 +1000

    Interim update 1 | Implement connection

--- src/lib/Connection.js
+++ src/lib/Connection.js
@@ -1,46 +1,100 @@
-const _ConnectionCommons = {
-  write: function (write, packet) {
-    let payload = packet.toString()
+class PacketUtils {
+  constructor(parent, connection, writerFn) {
+    this._parent = parent;
+    this._connection = connection;
+
+    this._buffer = Buffer.alloc(0);
+
+    // TODO: Add buffer temps
+
+    this._writerFn = writerFn;
+
+    // this.readerFn = readerFn;
+  }
+
+  write(packet) {
+    console.log("Write", packet);
+
+    let payload = packet.toString();
 
     // Write the payload length header
-    let payloadSize = Buffer.alloc(2)
-    payloadSize.writeUIntLE(payload.length)
+    let payloadSize = Buffer.alloc(2);
+    payloadSize.writeUInt16LE(payload.length);
 
-    console.log(payloadSize)
-    write(payloadSize + payload)
+    console.log(payloadSize);
+    this._writerFn(payloadSize + payload);
+  }
+
+  read(data) {
+    console.log("Read", data);
+
+    // console.log(this.buffer);
+
+    this._connection.emit("TEST", 123456789);
+
+    this._parent.emit("TEST", 987654321);
+
+    return;
+    // data = Buffer.from(data);
+
+    // readerEvt
   }
 }
 
-function ConnectionClient (clientClass) {
-  class ConnectionClient extends clientClass {
-    connect (port, host, callback) {
-      function callbackWrapper () {
-        this.__write__ = this.write.bind(this)
-        this.write = (...args) => _ConnectionCommons.write(this.__write__, ...args)
+// **For payloads up to 65535 bytes**
+// ```
+// |   2 bytes   | n bytes |
+// | length (LE) | payload |
+// ```
 
-        if (callback) {
-          callback.bind(this)()
-        }
+// **For payloads greater than 65535 bytes**
+// ```
+// |   2 bytes   |         1 byte         |   m bytes   | n bytes |
+// |  0x00 0x00  | length of length bytes | length (LE) | payload |
+// ```
+
+function ConnectionClient(clientClass) {
+  class ConnectionClient extends clientClass {
+    connect(port, host, callback) {
+      function callbackWrapper() {
+
+        this.__write__ = this.write.bind(this);
+
+        this._packetUtils_ = new PacketUtils(this, this, this.__write__);
+        // this._packetUtils_.read;
+
+        this.write = (...args) => this._packetUtils_.write(...args);
+        // this.on("data", this._packetUtils_.read);
+
+        callback && callback.bind(this)();
       }
 
-      return super.connect(port, host, callbackWrapper)
+      return super.connect(port, host, callbackWrapper);
     }
   }
 
-  return ConnectionClient
+  return ConnectionClient;
 }
 
-class ConnectionServer {
-  constructor (serverClass, ...args) {
-    class Server extends serverClass {
+function ConnectionServer(serverClass) {
+  console.log("ConnectionServer factory");
+  class ConnectionServer extends serverClass {
+    constructor() {
+      super(...arguments);
+      console.log("SERVER INIT");
 
+      this.on("connection", function(socket) {
+        this._packetUtils_ = new PacketUtils(this, socket, this.write);
+        this.write = (...args) => this._packetUtils_.write(...args);
+        socket.on("data", this._packetUtils_.read.bind(this._packetUtils_));
+      });
     }
-
-    return new Server(...args)
   }
+
+  return ConnectionServer;
 }
 
 module.exports = {
   ConnectionClient: ConnectionClient,
   ConnectionServer: ConnectionServer
-}
+};
