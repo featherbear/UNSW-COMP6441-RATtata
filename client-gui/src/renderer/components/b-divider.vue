034f2b1e16239deb5db2ffdd6292c8b90b9109c4
Andrew Wong <andrew.j.wong@outlook.com>
2019-07-21 04:02:24 +1000

    Bulma Divider - Vue

---
+++ client-gui/src/renderer/components/b-divider.vue
@@ -0,0 +1,13 @@
+<template>
+  <div :class="vertical ? 'is-divider-vertical' : 'is-divider'" :data-content="label"></div>
+</template><style>
+</style>
+<script>
+import "bulma-divider/dist/css/bulma-divider.min.css";
+export default {
+  props: {
+    label: String,
+    vertical: Boolean
+  }
+};
+</script>
