f5cd626e7eb9514dd7bcf66ab5c59540f5c56953
Andrew Wong <andrew.j.wong@outlook.com>
2019-07-22 02:13:25 +1000

    Stub Vuex connections

--- client-gui/src/renderer/store/modules/Connections.js
+++ client-gui/src/renderer/store/modules/Connections.js
@@ -1,23 +1,64 @@
 const state = {
   connections: {
-    1: {
+    123456: {
+      id: '123456',
       name: 'Paperweight',
-      os: 'mac'
+      address: '127.0.0.1',
+      os: 'mac',
+      data: {}
     },
-    2: {
+    234567: {
+      id: '234567',
       name: 'Blue Screen',
-      os: 'windows'
+      address: '127.0.0.1',
+      os: 'windows',
+      data: {}
+      //   platform: 'win32',
+      //   distro: 'winXTREME',
+      //   kernel: 'note sure',
+      //   hostname: 'localHOST',
+      //   logofile: 'logx',
+
+      //   hasDisplay: true,
+      //   lanIPs: ['127.0.0.1', '192.168.123.1'],
+      //   memoryPercentage: 1,
+      //   memoryUsed: 1,
+      //   memoryTotal: 100,
+      //   memorySuffix: 'TB',
+      //   cpu_load: 30
+      // }
     },
-    3: {
+    345678: {
+      id: '345678',
       name: 'rm -rf /',
-      os: 'linux'
+      address: '127.0.0.1',
+      os: 'linux',
+      data: {
+        platform: String,
+        distro: String,
+        kernel: String,
+        hostname: String,
+        logofile: String,
+
+        hasDisplay: Boolean,
+        lanIPs: Array[String],
+        memoryPercentage: Number,
+        memoryUsed: Number,
+        memoryTotal: Number,
+        memorySuffix: String,
+        cpu_load: Number
+      }
     }
   }
 }
 
-const mutations = {}
+const mutations = {
 
-const actions = {}
+}
+
+const actions = {
+
+}
 
 export default {
   state,
