80223dad3dd98c254b3511df1f4deb7c8694fd71
Andrew Wong <andrew.j.wong@outlook.com>
2019-07-21 19:19:15 +1000

    Section card template

---
+++ client-gui/src/renderer/pages/ClientConnection/_Section.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-collapse class="card" aria-id="contentIdForA11y3">
+    <div
+      slot="trigger"
+      slot-scope="props"
+      class="card-header"
+      role="button"
+      aria-controls="contentIdForA11y3"
+    >
+      <p class="card-header-title">{{title}}</p>
+      <a class="card-header-icon">
+        <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
+      </a>
+    </div>
+    <div class="card-content">
+      <div class="content">
+        <slot></slot>
+      </div>
+    </div>
+  </b-collapse>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+};
+</script>
